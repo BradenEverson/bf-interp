73dc308e8a75a02af59b7f3b709f7689b749e1b2
BradenEverson <bradeneverson@gmail.com>
2024-12-09 06:51:37 -0600

    Create REPL environment

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,43 @@ use std::{
 
 fn main() {
     let args: Vec<_> = std::env::args().collect();
-    if args.len() < 1 {
-        panic!("Error! No file target found :(");
-    }
-
-    let mut input = String::new();
-    let mut file = File::open(&args[1]).expect("Invalid file provided :(");
-    file.read_to_string(&mut input)
-        .expect("Failed to write to buffer");
-
     let mut curr_instance = BfInstance::default();
-    curr_instance.eval(&input);
+
+    if args.len() <= 1 {
+        println!("Entering BF REPL Environment");
+
+        loop {
+            let mut buffer = String::new();
+            print!("> ");
+            let _ = std::io::stdout().flush();
+
+            std::io::stdin()
+                .read_line(&mut buffer)
+                .expect("Failed to read input");
+
+            match buffer.trim() {
+                "exit" => {
+                    println!("Exiting BF REPL.");
+                    break;
+                }
+                "reset" => {
+                    curr_instance = BfInstance::default();
+                }
+                "" => continue,
+                _ => {}
+            }
+
+            curr_instance.eval(buffer.trim());
+            println!("\t\t\t\t\t\tCurrent Cell: {}", curr_instance.get());
+        }
+    } else {
+        let mut input = String::new();
+        let mut file = File::open(&args[1]).expect("Invalid file provided :(");
+        file.read_to_string(&mut input)
+            .expect("Failed to write to buffer");
+
+        curr_instance.eval(&input);
+    }
 }
 
 pub struct BfInstance {
